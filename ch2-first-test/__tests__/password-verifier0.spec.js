1311cbcc2364665b77d2f80c68afcfe2cdbcad7c
이진우 <leejinwoo1126@gmail.com>
2025-01-26 22:18:48 +0900

    toMatch() -> toContain() 변경, 테스트 제목 변경

--- ch2-first-test/__tests__/password-verifier0.spec.js
+++ ch2-first-test/__tests__/password-verifier0.spec.js
@@ -1,12 +1,9 @@
 const { verifyPassword } = require('../password-verifier0');
 	
-test('badly named test', () => {
-  // 테스트 준비-함수에 전달할 매개변수를 초기화한다
+test('verifyPassword, given a failing rule, returns errors', () => {
   const fakeRule = (input) => ({passed: false, reason: 'fake reason'});
 
-  // 함수 실행의 인자로 전달한다
   const errors = verifyPassword('any value', [fakeRule]);
 
-  // 함수의 반환 값을 기대 값과 비교한다
-  expect(errors[0]).toMatch('fake reason');
+  expect(errors[0]).toContain('fake reason');
 });
