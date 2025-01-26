5705f6fc38a4b66ebdea8651e9337b1380ae5c60
이진우 <leejinwoo1126@gmail.com>
2025-01-26 21:56:15 +0900

    2장 예제 생성

---
+++ ch2-first-test/__tests__/password-verifier0.spec.js
@@ -0,0 +1,6 @@
+test('badly named test', () => {
+  const fakeRule = input => ({passed: false, reason: 'fake reason'});
+  const errors = verifyPassword('any value', [fakeRule]);
+
+  expect(errors[0]).toMatch('fake reason');
+});
