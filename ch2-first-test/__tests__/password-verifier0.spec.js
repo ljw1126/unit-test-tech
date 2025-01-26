e614ed6dd150d3879f12d7b7c8b51d7541dacf67
이진우 <leejinwoo1126@gmail.com>
2025-01-26 22:36:10 +0900

    describe()로 테스트 그룹화

--- ch2-first-test/__tests__/password-verifier0.spec.js
+++ ch2-first-test/__tests__/password-verifier0.spec.js
@@ -1,9 +1,14 @@
 const { verifyPassword } = require('../password-verifier0');
 	
-test('verifyPassword, given a failing rule, returns errors', () => {
-  const fakeRule = (input) => ({passed: false, reason: 'fake reason'});
+describe('verifyPassword', () => {
+    describe('with a failing rule', () => {
+        const fakeRule = () => ({passed: false, reason: 'fake reason'});
+        
+        test('returns errors', () => {
 
-  const errors = verifyPassword('any value', [fakeRule]);
+          const errors = verifyPassword('any value', [fakeRule]);
 
-  expect(errors[0]).toContain('fake reason');
+          expect(errors[0]).toContain('fake reason');
+        });
+    });
 });
