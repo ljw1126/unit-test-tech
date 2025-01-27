e2f06f68c0db947c742f3a57959adc60d7b5b054
이진우 <leejinwoo1126@gmail.com>
2025-01-27 11:02:28 +0900

    예제2-13

--- ch2-first-test/__tests__/password-verifier1.spec.js
+++ ch2-first-test/__tests__/password-verifier1.spec.js
@@ -1,18 +1,26 @@
 const { PasswordVerifier1 } = require('../password-verifier1');
 
 describe('PasswordVerifier', () => {
+  let verifier;
+  beforeEach(() => (verifier = new PasswordVerifier1()));
+
   describe('with a failing rule', () => {
+     let fakeRule, errors;
+     beforeEach(() => {
+       fakeRule = () => ({ passed: false, reason: 'fake reason' });
+       verifier.addRule(fakeRule);
+     });
+
      it('has an error message based on the rule.reason', () => {
-        const verifier = new PasswordVerifier1();
-        const fakeRule = () => ({
-           passed: false,
-           reason: 'fake reason'
-        });
-        
-        verifier.addRule(fakeRule);
-        const errors = verifier.verify('any value');
+        errors = verifier.verify('any value');
 
         expect(errors[0]).toContain('fake reason');
+     });
+
+     it('has exactly one error', () => {
+         errors = verifier.verify('any value');
+
+         expect(errors.length).toBe(1);
      });
   });
 });
