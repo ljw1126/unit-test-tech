a17a491df19aa4298b21bac1f5fed216568d8ade
이진우 <leejinwoo1126@gmail.com>
2025-01-27 11:15:02 +0900

    예제2-15

--- ch2-first-test/__tests__/password-verifier1.spec.js
+++ ch2-first-test/__tests__/password-verifier1.spec.js
@@ -21,4 +21,41 @@ describe('PasswordVerifier', () => {
          expect(errors.length).toBe(1);
      });
   });
+
+  describe('with a passing rule', () => {
+     let fakeRule, errors;
+
+     beforeEach(() => {
+         fakeRule = () => ({ passed : true, reason : ''});
+         verifier.addRule(fakeRule);
+         errors = verifier.verify('any value');
+     });
+
+     it('has no errors', () => {
+       expect(errors.length).toBe(0);
+     });
+  });
+
+  describe('with a failing and a passing rule', () => {
+     let fakeRulePass, fakeRuleFail, errors;
+     beforeEach(() => {
+        fakeRulePass = () => ({passed: true, reason: 'fake success'});
+        fakeRuleFail = () => ({passed: false, reason: 'fake reason'});
+        verifier.addRule(fakeRulePass);
+        verifier.addRule(fakeRuleFail);
+        errors = verifier.verify('any value');
+     });
+
+     it('has one error', () => {
+        expect(errors.length).toBe(1);
+     });
+
+     it('error text belongs to failed rule', () => {
+        expect(errors[0]).toContain('fake reason');
+     });
+  });
+
+
+
 });
+
